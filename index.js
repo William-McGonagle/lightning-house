201b51cfba37f40243cd5954c7102a36f31ffefb
knuf <knuf@gofuckyourself.com>
2020-06-11 15:06:13 -0400

    added key based encryption

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-var ui = require("./ui.js");
-var irc = require('irc');
-var readline = require('readline');
+const ui = require("./ui.js");
+const irc = require('irc');
+const readline = require('readline');
+const NodeRSA = require('node-rsa');
+
+var myKey = null;
+var theirKey = null;
 
 var channels = [];
-var currentChannel = "#Node.js";
+var currentChannel = "";
 
 var rl = readline.createInterface({
   input: process.stdin,
@@ -11,16 +15,35 @@ var rl = readline.createInterface({
   terminal: false
 });
 
-var client = new irc.Client('irc.uk.mibbit.net', 'mamamama', {});
+var client = new irc.Client(process.argv[2], process.argv[3], {});
 
 ui.clearScreen();
-ui.drawLogo();
 ui.displayChannels(channels, channels.indexOf(currentChannel));
 
 ui.helpBar();
 ui.bottomBar();
 
 client.addListener('pm', function (from, message) {
+
+  var actualMessage = "";
+
+  if (message.startsWith("-----BEGIN PUBLIC KEY-----")) {
+
+    theirKey = new NodeRSA().importKey(message.replace(/!/gm, "\n"), 'pkcs8');
+
+  } else {
+
+    if (myKey == null) {
+
+      actualMessage = message;
+
+    } else {
+
+      actualMessage = userKey.decrypt(message, 'utf8');
+
+    }
+
+  }
 
   ui.clearScreen();
   ui.displayChannels(channels, channels.indexOf(currentChannel));
@@ -47,6 +70,29 @@ rl.on('line', function(line) {
         ui.bottomBar();
 
         break;
+      case ":pubkey":
+
+        client.say(currentChannel, myKey.exportKey('pkcs8-public-pem').replace(/\n/g, "!"));
+
+        ui.clearScreen();
+        ui.displayChannels(channels, channels.indexOf(currentChannel));
+        ui.notice("sent public key to channel", currentChannel);
+        ui.displayMessages(currentChannel);
+        ui.helpBar();
+        ui.bottomBar();
+
+        break;
+      case ":genkey":
+
+        myKey = new NodeRSA({b: 1024})
+
+        ui.clearScreen();
+        ui.displayChannels(channels, channels.indexOf(currentChannel));
+        ui.displayMessages(currentChannel);
+        ui.helpBar();
+        ui.bottomBar();
+
+        break;
       case ":open":
 
         currentChannel = line.split(" ").slice(1, line.split(" ").length).join(" ").toLowerCase();
@@ -57,13 +103,34 @@ rl.on('line', function(line) {
 
         }
 
-        client.addListener('message#' + currentChannel, function (from, to, message) {
+        client.addListener('message' + currentChannel, function (from, to, message) {
+
+          var stringMessage = message.args[1];
+          var actualMessage = "";
+
+          if (stringMessage.startsWith("-----BEGIN PUBLIC KEY-----")) {
+
+            theirKey = new NodeRSA().importKey(stringMessage.replace(/!/gm, "\n"), 'pkcs8-public-pem');
+
+          } else {
+
+            if (myKey == null) {
+
+              actualMessage = stringMessage;
+
+            } else {
+
+              actualMessage = myKey.decrypt(stringMessage, 'utf8');
+
+            }
+
+          }
 
           ui.clearScreen();
           ui.displayChannels(channels, channels.indexOf(currentChannel));
           ui.showNotification(0, `Message From ${from}`);
           ui.helpBar();
-          ui.message(from, message, currentChannel);
+          ui.message(from, actualMessage, currentChannel);
           ui.displayMessages(currentChannel);
           ui.bottomBar();
 
@@ -88,7 +155,16 @@ rl.on('line', function(line) {
 
   } else {
 
-    client.say(currentChannel, line);
+    if (theirKey != null) {
+
+      var encrypted = theirKey.encrypt(line, 'base64');
+      client.say(currentChannel, encrypted);
+
+    } else {
+
+      client.say(currentChannel, line);
+
+    }
 
     ui.clearScreen();
     ui.displayChannels(channels, channels.indexOf(currentChannel));
